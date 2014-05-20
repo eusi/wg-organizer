ac49f5098a5e3786ad3517219c2daaab0ca54c10
eusi <eusi.cf@gmail.com>
2014-05-20 10:50:11 +0200

    Optimized UI 1st draft and added a start-site and logged-in-site.

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,8 @@
 class TestController < ActionController::Base
-  def index
+	layout 'loggedin'
+	
+	def index
     # put any code here that you need 
     # (although for a static view you probably won't have any)
-  end
+end
 end
