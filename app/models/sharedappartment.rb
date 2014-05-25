70dd6633b4791a253a6dff72b36a30d2e2c48206
wernbaer <wernerhuttner@googlemail.com>
2014-05-25 20:03:27 +0200

    get_balance() in user und sharedappartment

--- app/models/sharedappartment.rb
+++ app/models/sharedappartment.rb
@@ -2,4 +2,22 @@ class Sharedappartment < ActiveRecord::Base
 validates_presence_of :Name
   validates_presence_of :Password
   has_many :Users
+  
+  def get_user()
+	allusers=User.where(:Sharedappartment=>self)
+  end
+  
+
+  def get_balance()
+	
+	balances=Hash.new
+	get_user().each do |current_user|
+	
+	result = Completedtask.where(:ByUser=>current_user).sum(:Credits)-Charge.where(:ForUser=>current_user).sum(:Credits)	
+	balances[current_user]=result
+	end
+	balances
+	
+end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ has_many :Completedtasks
 has_many :Charges
 belongs_to :Userrole
 belongs_to :Sharedappartment
+
+def get_balance()
+	
+	
+	result = Completedtask.where(:ByUser=>self).sum(:Credits)-Charge.where(:ForUser=>self).sum(:Credits)	
+	
+end
+
 end
