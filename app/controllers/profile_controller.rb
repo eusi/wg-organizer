cd9e2a722f5ca1dbdca3270fa339a8d3b0292aa0
Patrick Krempels <Patrick@Patricks-MacBook.local>
2014-05-29 20:41:33 +0200

    create und join wg korrigiert

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,7 +5,23 @@ class ProfileController < ActionController::Base
     end
     
     def createWG
-    	@sharedappartment = Sharedappartment.create_sharedappartment(user,name,password)    	 	
+    	if(current_user.Sharedappartment_id==nil)
+    		@sharedappartment = Sharedappartment.create_sharedappartment(current_user,params[:name],params[:password])
+    		redirect_to '/profile', :notice => "Succesfully created new sharedappartment"
+    		
+    	else
+    		redirect_to '/profile', :notice => "Leave your sharedappartment before creating a new one"
+    	end
+    	    	 	
+    end
+    
+    def joinWG
+    	@sharedappartment= Sharedappartment.join_sharedappartment(current_user,params[:name],params[:password])
+    	redirect_to '/profile', :notice => "Welcome to your sharedappartment!"
+    end
+    
+    def edit
+    	
     end
 
 end
