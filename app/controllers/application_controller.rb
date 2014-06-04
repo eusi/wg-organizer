c07603a3ebb3909559f43e17c2286aac4622f151
eusi <eusi.cf@gmail.com>
2014-06-05 00:33:21 +0200

    added Picture to Profile, added a more minor cancel-account button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,41 @@
 class ApplicationController < ActionController::Base
 
-  layout 'home'
-  protect_from_forgery with: :exception
+	layout 'home'
+	protect_from_forgery with: :exception
 
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+	before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+	protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:UserName, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :UserName, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:UserName, :email, :password, :password_confirmation, :current_password) }
-  end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:UserName, :email, :password, :password_confirmation, :remember_me) }
+		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :UserName, :email, :password, :remember_me) }
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:UserName, :email, :Picture, :password, :password_confirmation, :current_password) }
+	end
 
-  protect_from_forgery
+	protect_from_forgery
 
-  private
+	private
 
-  def after_sign_in_path_for(resource)
-    if current_user.Sharedappartment_id != nil
-      '/main'
-    else 
-      '/profile'
-    end
-  end
+	def after_sign_in_path_for(resource)
+		if current_user.Sharedappartment_id != nil
+			'/main'
+		else 
+			'/profile'
+		end
+	end
 
-  def after_sign_up_path_for(resource)
-    '/profile'
-  end
+	def after_sign_up_path_for(resource)
+		'/profile'
+	end
 
-  def after_sign_out_path_for(resource_or_scope)
-    '/home'   #new_user_session_path    #root_path
-  end
+	def after_sign_out_path_for(resource_or_scope)
+    	'/home'   #new_user_session_path    #root_path
+	end
+
+	def after_update_path_for(resource)
+		'/profile'
+	end
 
 end
