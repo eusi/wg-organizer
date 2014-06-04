a30df2f7402c8e161ce48981371171697f3d318e
Patrick Krempels <Patrick@Patricks-MacBook.local>
2014-06-04 09:56:55 +0200

    ceate_shout action erstellt

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ActionController::Base
     	if(current_user.Sharedappartment_id==nil)
     		redirect_to '/profile'
     	end
-		#@shouts = Shout.all_shouts
+      @shouts = current_user.get_shoutbox_messages_of_appartment(1)
 	end
 
 	def create
@@ -18,6 +18,32 @@ class MainController < ActionController::Base
 	  else
 	    render :action => 'index'
 	  end
-	end
+  end
+
+  def last_activities
+    @completedtasks = current_user.get_last_activities_of_appartment
+  end
+
+  def appartment_balance
+    dictionary = current_user.Sharedappartment.get_balance
+  end
+
+  def create_shout
+    s = Shoutboxmessage.new()
+    s.Message = params[:shoutboxmessage][:Message]
+    s.User_id = current_user.id
+    if s.save
+      redirect_to :action => 'index'
+      flash[:notice] = 'Thanks for shouting!'
+
+    else
+      redirect_to :action => 'index'
+      flash[:alert] = 'Uups, something went wrong!'
+    end
+  end
+
+
+
+
 
 end
