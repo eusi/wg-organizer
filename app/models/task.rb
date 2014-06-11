9fd16c0ea41df97bdc61b99e7234afd03cf88abd
eusi <eusi.cf@gmail.com>
2014-06-11 18:28:46 +0200

    Minor gui fixes.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,5 +6,4 @@ validates :task_name, presence: true
 validates :credit_per_unit, presence: true
 validates :base_credit, presence: true
 validates :task_description, length: { maximum: 2000 }
-
 end
