630161a134eaaa872b2c2a55864345bcf91ed5e0
Patrick Krempels <Patrick@Patricks-MacBook.local>
2014-06-12 17:01:09 +0200

    job controller fehlerhandling einkommentiert

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -5,25 +5,27 @@ class JobController < ActionController::Base
   end
 
   def create_job
-    #begin
-    @completedtask = Completedtask.complete_task( current_user, 
+    begin
+      @completedtask = Completedtask.complete_task( current_user,
                                                   params[:completedtask][:for_users],
                                                   params[:completedtask][:amount].to_i,
                                                   Task.find(params[:completedtask][:task_id]),
                                                   params[:completedtask][:task_start],
                                                   params[:completedtask][:task_end],
                                                   params[:completedtask][:summary])
-    redirect_to '/job', :notice =>'Jobs done!'
-    #rescue => error
-     #@completedtask = nil
-    #end
-    #if @sharedappartment
-      #redirect_to '/job', :notice =>'Jobs done!'
-    #else
-      #redirect_to '/jobs', :alert => error.message
-    #end
+
+    rescue => error
+      @completedtask = nil
+    end
+
+    if @sharedappartment
+      redirect_to '/job', :notice =>'Jobs done!'
+    else
+      redirect_to '/jobs', :alert => error.message
+    end
 
   end
+
 
   def get_roomies
     @users = current_user.Sharedappartment.Users
