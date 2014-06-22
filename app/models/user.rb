3f36f69d1402590936263f651e032f7adfab8311
Patrick Krempels <Patrick@Patricks-MacBook.local>
2014-06-22 12:49:43 +0200

    timeout-funktion korrigiert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, and :omniauthable
+  devise :database_authenticatable, :registerable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :Shoutboxmessages, :dependent => :delete_all
 	has_many :Completedtasks, :dependent => :delete_all
